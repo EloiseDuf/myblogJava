5599324ea42f2ab894e1b85b99be44b82c4a68c5
ELOISE DUFAU <eloise.dufau@gmail.com>
2025-01-13 16:23:14 +0100

    quete 3 ok

---
+++ src/main/java/org/wildcodeschool/myblog/repository/ArticleRepository.java
@@ -0,0 +1,6 @@
+package org.wildcodeschool.myblog.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.wildcodeschool.myblog.model.Article;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
