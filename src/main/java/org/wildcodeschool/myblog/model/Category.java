dd00eb68ec3b758be6a3b87e58064cb218ce3f38
ELOISE DUFAU <eloise.dufau@gmail.com>
2025-01-14 17:06:28 +0100

    quête 5 ok

--- src/main/java/org/wildcodeschool/myblog/model/Category.java
+++ src/main/java/org/wildcodeschool/myblog/model/Category.java
@@ -1,7 +1,6 @@
 package org.wildcodeschool.myblog.model;
 
 import jakarta.persistence.*;
-import org.springframework.context.annotation.EnableMBeanExport;
 
 
 @Entity
--- src/main/java/org/wildcodeschool/myblog/repository/CategoryRepository.java
+++ src/main/java/org/wildcodeschool/myblog/repository/CategoryRepository.java
@@ -1,11 +1,8 @@
 package org.wildcodeschool.myblog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.wildcodeschool.myblog.model.Article;
 import org.wildcodeschool.myblog.model.Category;
 
-import java.util.List;
-import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
