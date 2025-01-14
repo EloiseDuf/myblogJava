6df845a6a15071beb08cf317f850250f73a40858
ELOISE DUFAU <eloise.dufau@gmail.com>
2025-01-14 17:05:03 +0100

    quête 5 ok

--- src/main/java/org/wildcodeschool/myblog/controller/ArticleController.java
+++ src/main/java/org/wildcodeschool/myblog/controller/ArticleController.java
@@ -3,7 +3,6 @@ package org.wildcodeschool.myblog.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.wildcodeschool.myblog.model.Article;
 import org.wildcodeschool.myblog.model.Category;
